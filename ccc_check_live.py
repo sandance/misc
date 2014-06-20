6db56cf350be448b49c8de49a9967df1f03007b4
sandance <rashed.rashedislam@gmail.com>
2014-06-20 13:02:09 -0400

    after adding ccc live count check

---
+++ ccc_check_live.py
@@ -0,0 +1,59 @@
+import sys
+import re
+
+def do_work(filename):
+	data= []
+	fp = open(filename)
+	for line in fp:
+		line = line.strip().split(' ')
+		data.append(line)
+	return data
+	
+def get_count(data):
+	switch_check=re.compile(r"[a-zA-Z0-9_]")
+        end_line = re.compile(r"[a-zA-Z]")	
+	final_output = list()
+	for k,i  in enumerate(data):
+		if switch_check.search(i[0]) and not i[1].find('2014'):
+			#print "match found"
+			sw = list()
+			final_output.append(i[0])
+			final_output.append(i[1])
+		else:
+			if i[1]=='0':
+				count_0 = int(i[0])
+				#print "count 0 %d" % count_0
+				sw.append(int(i[0]))
+				final_output.append(count_0)
+			elif i[1]=='error_code':
+				#print "Found error code"
+				final_output.append(sum(sw[:]))
+				flag=1
+				if 'pcmd' in final_output[0]:
+					flag=2
+				percent=float(sw[0]) * 100.0 / (flag * float(sum(sw[:])))
+				final_output.append(round(percent,2))
+				del sw[:]
+			#	print "This is what is want"
+			#	print i[1]
+			elif k==2 and i[1]=='502':
+				sw.append(int(i[0]))
+				final_output.append(sum(sw[:]))
+				flag=1
+                                if 'pcmd' in final_output[0]:
+                                        flag=2
+                                percent=float(sw[0]) * 100.0 / (flag * float(sum(sw[:])))
+                                final_output.append(round(percent,2))
+				del sw[:]
+			else:
+				sw.append(int(i[0]))
+	#print final_output
+	return final_output
+				 			
+if __name__=='__main__':
+	datalist = do_work(sys.argv[1])
+	final_data=get_count(datalist)
+	print final_data
+			
+	# now calculate percentage
+	
