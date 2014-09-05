1ca87c04503b51b43ec776e0b704a6cc12d0145c
sandance <rashed.rashedislam@gmail.com>
2014-09-05 17:43:50 -0400

    assigment modified

---
+++ testing.old/script/disk_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BRICKS=$@
+
+HOST_IP=$(/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+HOSTNAME=$(hostname) # not required actually
+
+
+#Now for each gluster brick get the disk space
+echo -e "Filesystem\tSize Used Avail\tDisk\t\tUse%"
+
+for gsbrick in $BRICKS
+do
+	percent=$(df -h $gsbrick/ | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5,$1 }' | cut -d'%' -f1)
+	alex_info=$(df -h $gsbrick/  | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $2,$3,$4}')
+	drive=$(df -h $gsbrick/ | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $1 }')
+	echo -e "$HOST_IP\t$alex_info\t$drive\t$percent"
+done
